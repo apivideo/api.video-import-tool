f6004c0871844104cc9ff65532919cabca6daba9
AyaBochman <aya.bochman@gmail.com>
2022-11-29 14:34:43 +0100

    Styled stepper and homepage WIP

--- src/components/MigrationTool.tsx
+++ src/components/MigrationTool.tsx
@@ -1,85 +1,139 @@
-import Video from '@api.video/nodejs-client/lib/model/Video'
-import Link from 'next/link'
-import { useState } from 'react'
-import Authenticate from '../components/Authenticate'
-import ImportProgress from '../components/ImportProgress'
-import Stepper from '../components/stepper/Stepper'
-import VideoSourceSelector from '../components/VideoSourceSelector'
-import { ProviderName, Providers } from '../providers'
-import { AuthenticationContext } from '../types/common'
-
+import Video from '@api.video/nodejs-client/lib/model/Video';
+import Link from 'next/link';
+import { useState } from 'react';
+import Authenticate from '../components/Authenticate';
+import ImportProgress from '../components/ImportProgress';
+import Stepper from '../components/stepper/Stepper';
+import VideoSourceSelector from '../components/VideoSourceSelector';
+import { ProviderName, Providers } from '../providers';
+import { AuthenticationContext } from '../types/common';
+import Image from 'next/image'
 interface MigrationToolProps {
   providerName?: ProviderName;
 }
 
 const buildId = (length: number) => {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
 
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
 
 const MigrationTool: React.FC<MigrationToolProps> = (props) => {
   const [step, setStep] = useState<number>(props.providerName ? 1 : 0);
   const [importedVideos, setImportedVideos] = useState<Video[]>();
   const [migrationId, _] = useState<string>(buildId(9));
-  const [authenticationContext, setAuthenticationContext] = useState<AuthenticationContext>();
+  const [authenticationContext, setAuthenticationContext] =
+    useState<AuthenticationContext>();
 
-  const provider = props.providerName ? Providers[props.providerName] : undefined;
+  const provider = props.providerName
+    ? Providers[props.providerName]
+    : undefined;
 
-  const providers = Object.keys(Providers)
-    .map((provider: ProviderName) => ({
-      link: `/${(provider as string).toLowerCase()}`,
-      displayName: Providers[provider].displayName,
-      name: provider,
-      color: Providers[provider].color
-    }));
+  const providers = Object.keys(Providers).map((provider: ProviderName) => ({
+    link: `/${(provider as string).toLowerCase()}`,
+    displayName: Providers[provider].displayName,
+    name: provider,
+    color: Providers[provider].color,
+  }));
 
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">{provider ? provider.title : "Video migration tool"}</h1>
-        <Stepper activeStep={step} steps={["Select source", "Authentication", "Video Selection", "Import Progress"]}></Stepper>
+    <div className="py-36">
+      <main className="border border-slate-200 rounded-lg w-fit p-8">
+        <div>
+          <div className="flex justify-between pb-4">
+            <h1 className="text-left text-2xl font-semibold">
+              {provider ? provider.title : 'Video migration tool'}
+            </h1>
+            <button className="flex items-center gap-2 border rounded-md bg-black text-white text-sm font-semibold py-1">
+            <Image src="/github.svg" height={14} width={14} alt='github' />
+              View on Github
+            </button>
+          </div>
 
-        <div id="content">
-          {step === 0 && <div style={{ display: "flex", flexDirection: "row" }}>
-            {providers.map(({ displayName, link, name, color }) => <div style={{ flex: 1, textAlign: "center" }} key={name}>
-              <Link style={{ flex: 1, margin: "0 10px", backgroundColor: color, color: "white", borderRadius: 6, padding: "10px 0", textDecoration: "none" }} href={link} >{displayName}</Link>
-            </div>)}
-          </div>}
-
-          {step === 1 && <Authenticate
-            providerName={props.providerName}
-            onSubmit={async (authenticationContext: AuthenticationContext) => {
-              setAuthenticationContext(authenticationContext);
-              setStep(2);
-            }}
-          />}
-
-          {step === 2 && <VideoSourceSelector
-            migrationId={migrationId}
-            providerName={props.providerName!}
-            authenticationContext={authenticationContext!}
-            onSubmit={(videos) => {
-              setImportedVideos(videos);
-              setStep(3);
-            }}
-          />}
-
-          {step === 3 && <ImportProgress
-            apiVideoApiKey={authenticationContext?.apiVideoApiKey!}
-            videos={importedVideos || []}
-          />}
+          <p className="text-sm">
+            This tool helps you quickly import your already hosted videos to
+            api.video and take advantage of our API with your existing content.
+          </p>
+          <Stepper
+            activeStep={step}
+            steps={[
+              'Select source',
+              'Authentication',
+              'Video Selection',
+              'Import Progress',
+            ]}
+          ></Stepper>
         </div>
-        {step === 0 && <p><Link href="/migrations">Your previous migrations</Link></p>}
+        <div className="h-px w-full bg-slate-300"></div>
+        <div id="content">
+          {step === 0 && (
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              {providers.map(({ displayName, link, name, color }) => (
+                <div style={{ flex: 1, textAlign: 'center' }} key={name}>
+                  <Link
+                    style={{
+                      flex: 1,
+                      margin: '0 10px',
+                      backgroundColor: color,
+                      color: 'white',
+                      borderRadius: 6,
+                      padding: '10px 0',
+                      textDecoration: 'none',
+                    }}
+                    href={link}
+                  >
+                    {displayName}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+
+          {step === 1 && (
+            <Authenticate
+              providerName={props.providerName}
+              onSubmit={async (
+                authenticationContext: AuthenticationContext
+              ) => {
+                setAuthenticationContext(authenticationContext);
+                setStep(2);
+              }}
+            />
+          )}
+
+          {step === 2 && (
+            <VideoSourceSelector
+              migrationId={migrationId}
+              providerName={props.providerName!}
+              authenticationContext={authenticationContext!}
+              onSubmit={(videos) => {
+                setImportedVideos(videos);
+                setStep(3);
+              }}
+            />
+          )}
+
+          {step === 3 && (
+            <ImportProgress
+              apiVideoApiKey={authenticationContext?.apiVideoApiKey!}
+              videos={importedVideos || []}
+            />
+          )}
+        </div>
+        {step === 0 && (
+          <p>
+            <Link href="/migrations">Your previous migrations</Link>
+          </p>
+        )}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default MigrationTool
+export default MigrationTool;
--- src/components/stepper/Stepper.tsx
+++ src/components/stepper/Stepper.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { ChevronRight } from 'react-feather';
 
 interface StepperProps {
   activeStep: number;
@@ -6,32 +7,37 @@ interface StepperProps {
 }
 
 const Stepper: React.FC<StepperProps> = (props) => {
- 
   const getSteps = () => {
     const res = [];
-    for(let i=0 ; i<props.steps.length ; i++) {
+    for (let i = 0; i < props.steps.length; i++) {
       const step = props.steps[i];
-      
-      const classes = ["step"];
-      
-      if(i < props.activeStep) classes.push("done");
-      else if(i === props.activeStep) classes.push("active");
-
-      if(i > 0) {
-        const sepClasses = ["separator"];
-        if(i <= props.activeStep) sepClasses.push("done");
-        res.push(<div key={"sep" + i} className={sepClasses.join(" ")}></div>)
+      if (i > 0) {
+        res.push(
+          <div key={'step' + i}>
+            <ChevronRight size={'1.3rem'} strokeWidth={2.5} />
+          </div>
+        );
       }
 
-      res.push(<div key={"step" + i} className={classes.join(" ")}> 
-        <p className="circle">{i < props.activeStep ? "✓" : i+1}</p>
-        <p className="label" key={step}>{step}</p>
-      </div>);
+      res.push(
+        <div key={'step' + i} className="flex gap-2 align-middle">
+          <p
+            className={`rounded-full ${
+              i === props.activeStep ? 'bg-blue-500' : `${i < props.activeStep ? 'bg-green-600' : 'bg-gray-300'}`
+            } h-6 w-6 text-white text-center text-sm flex justify-center align-middle p-0.5`}
+          >
+            {i + 1}
+          </p>
+          <p className="text-base font-semibold" key={step}>
+            {step}
+          </p>
+        </div>
+      );
     }
     return res;
-  }
+  };
 
-  return (<div className="stepper">{getSteps()}</div>);
-}
+  return <div className="flex gap-4 py-4 align-middle">{getSteps()}</div>;
+};
 
 export default Stepper;
