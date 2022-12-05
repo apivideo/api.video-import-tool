d441d3b128323cd497c8d045f141f280be204d24
AyaBochman <aya.bochman@gmail.com>
2022-12-05 15:40:23 +0100

    auth step for dropbox and vimeo done + video selection WIP

--- src/components/provider-login/VimeoLogin.tsx
+++ src/components/provider-login/VimeoLogin.tsx
@@ -1,35 +1,49 @@
-import React, { useEffect } from "react";
-import { ProviderLoginProps } from "../../providers";
-
-
+import React, { useEffect } from 'react';
+import { ProviderLoginProps } from '../../providers';
 
 const VimeoLogin = (props: ProviderLoginProps) => {
-  const [vimeoAccessToken, setVimeoAccessToken] = React.useState<string>("");
+  const [vimeoAccessToken, setVimeoAccessToken] = React.useState<string>('');
 
   useEffect(() => {
-    const accessToken = sessionStorage.getItem("vimeoAccessToken") || "";
+    const accessToken = sessionStorage.getItem('vimeoAccessToken') || '';
     setVimeoAccessToken(accessToken);
     props.onAccessTokenChanged(accessToken);
   }, []);
 
   return (
-    <>
-      <div>
-        <label htmlFor="vimeoAccessToken">Enter your Vimeo access token <i><a target="_blank" href="/doc/generate-a-vimeo-access-token">how to generate your access token</a></i></label>
+    <div>
+      <div className="flex flex-col gap-4">
+        <label htmlFor="vimeoAccessToken" className="flex gap-4">
+          Enter your Vimeo access token
+          <i>
+            <a
+              target="_blank"
+              href="/doc/generate-a-vimeo-access-token"
+              className="text-blue-500 underline text-xs"
+            >
+              how to generate your access token
+            </a>
+          </i>
+        </label>
         <input
-          className={props.errorMessage ? "error" : ""}
+          className={`h-10 ${
+            props.errorMessage
+              ? 'outline outline-red-500 outline-2'
+              : 'outline outline-slate-300 rounded-lg shadow outline-1'
+          }`}
           id="vimeoAccessToken"
-          type={"password"}
+          type={'password'}
           value={vimeoAccessToken}
           onChange={(v) => {
             setVimeoAccessToken(v.target.value);
             props.onAccessTokenChanged(v.target.value);
-            sessionStorage.setItem("vimeoAccessToken", v.target.value);
-          }}></input>
-        <p className="inputError">{props.errorMessage}&nbsp;</p>
+            sessionStorage.setItem('vimeoAccessToken', v.target.value);
+          }}
+        ></input>
       </div>
-    </>
+      <p className="text-sm text-red-600">{props.errorMessage}&nbsp;</p>
+    </div>
   );
-}
+};
 
 export default VimeoLogin;
