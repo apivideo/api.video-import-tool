1586b84a5b2b9ba416c06155d82a9e75e9c55c10
Olivier Lando <olivier@api.video>
2022-12-19 16:46:30 +0100

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { GlobalContextProvider } from '../components/context/Global';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GlobalContextProvider>
     <Layout>
+        <GoogleAnalytics trackPageViews={{ ignoreHashChange: true }} />
         <Component {...pageProps} />
     </Layout>
     </GlobalContextProvider>
