8026ce44970145dc1f781c3601d993a00dd6b50d
AyaBochman <aya.bochman@gmail.com>
2023-01-12 18:15:17 +0100

    Added supported domain for images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['i.vimeocdn.com', 'cdn.api.video'],
+    domains: ['i.vimeocdn.com', 'cdn.api.video', 'vod.api.video'],
   },
 };
 
